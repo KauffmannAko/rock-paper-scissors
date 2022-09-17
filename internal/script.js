668592261ee440e5fc18d61a126417b044c72640
KauffmannAko <nanakauffmann@gmail.com>
2022-09-17 12:42:48 +0100

    added a function that randomly returns rock or paper or scissors

--- internal/script.js
+++ internal/script.js
@@ -1,1 +1,12 @@
-document.write('hello world!')
+/*
+ * store the three itemss in an array
+ * output random number from 0 to 2
+ * return a random item 
+  
+*/
+
+ function getComputerChoice(){
+    let randomItem = ['rock','paper','scissors'];
+    let i = Math.floor(Math.random()*(2 + 1));
+    return randomItem[i];
+ }
